8d23f652cce01c00dd7bbd89d9409bb9d0799d33
finaxys <cblampain@finaxys.com>
2018-06-26 15:27:53 +0200

    Added ListState example

--- src/test/CarSpeeds.java
+++ src/test/CarSpeeds.java
@@ -1,6 +1,8 @@
 package test;
 
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
@@ -29,23 +31,10 @@ public class CarSpeeds {
 		}
 
 		DataStream<String> averageViewStream = dataStream.map((String row) -> Tuple2.of(1, Double.parseDouble(row)))
-				.keyBy(0).flatMap((Tuple2<Integer, Double> input, Collector<String> out) -> {
-					ValueState<Tuple2<Integer, Double>> countSumState = null;
-					Tuple2<Integer, Double> currentCountSum = countSumState.value();
-					if (input.f1 >= 65) {
-						out.collect(String.format(
-								"Exceeded ! The average speed of the last %s car(s) was %s," + " your speed is %s",
-								currentCountSum.f0, currentCountSum.f1 / currentCountSum.f0, input.f1));
-						countSumState.clear();
-					} else {
-						out.collect("Thanks for staying under the speed limit !");
-					}
-					currentCountSum.f0 += 1;
-
-					currentCountSum.f1 += input.f1;
-
-					countSumState.update(currentCountSum);
-				});
+				.keyBy(0)
+				.flatMap(new AverageSpeedListState());
+		
+		averageViewStream.print();
 
 		env.execute("Count Window");
 
@@ -72,16 +61,48 @@ public class CarSpeeds {
 
 			countSumState.update(currentCountSum);
 		}
-		
+
 		public void open(Configuration config) {
-			ValueStateDescriptor<Tuple2<Integer, Double>> descriptor = 
-					new ValueStateDescriptor<Tuple2<Integer, Double>>
-						("carAverageSpeed", TypeInformation.of(new TypeHint<Tuple2<Integer,Double>>() {
-						}),Tuple2.of(0, 0.0));
-			
+			ValueStateDescriptor<Tuple2<Integer, Double>> descriptor = new ValueStateDescriptor<Tuple2<Integer, Double>>(
+					"carAverageSpeed", TypeInformation.of(new TypeHint<Tuple2<Integer, Double>>() {
+					}), Tuple2.of(0, 0.0));
+
 			countSumState = getRuntimeContext().getState(descriptor);
 		}
 
 	}
 
+	public static class AverageSpeedListState extends RichFlatMapFunction<Tuple2<Integer, Double>, String> {
+
+		private transient ListState<Double> speedListState;
+
+		@Override
+		public void flatMap(Tuple2<Integer, Double> input, Collector<String> out) throws Exception {
+			if (input.f1 >= 65) {
+				Iterable<Double> carSpeeds = speedListState.get();
+				int count = 0;
+				double sum = 0;
+
+				for (Double carSpeed : carSpeeds) {
+					count++;
+					sum += carSpeed;
+				}
+
+				out.collect(String.format(
+						"Exceeded ! The average speed of the last %s car(s) was %s," + " your speed is %s",
+						count, sum / count, input.f1));
+				speedListState.clear();
+			} else {
+				out.collect("Thanks for staying under the speed limit !");
+			}
+			
+			speedListState.add(input.f1);
+		}
+		
+		public void open(Configuration config) {
+			ListStateDescriptor<Double> descriptor = new ListStateDescriptor<>("carAverageSpeed", Double.class);
+			speedListState = getRuntimeContext().getListState(descriptor);
+		}
+
+	}
 }
